efef2fe05c5f075f150e29344f6feb906db09973
Sachin <sachin@Sachins-MacBook-Air.local>
2024-08-11 19:36:46 +0530

    router issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode:true,
-    basePath:'/telemune/'
+    basePath:'/telemune',
+    assetPrefix: '/telemune/', // Add this line
+    trailingSlash: true, // Add this line to ensure trailing slashes in paths
 };
 
 export default nextConfig;
